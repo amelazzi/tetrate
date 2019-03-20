410fc9a8631bb6848ec221295c504a5623cec526
Amel AZZI <ea_azzi@esi.dz>
2019-03-20 20:02:39 +0100

    add: basic testimonial -> home

---
+++ src/components/home/testimony.js
@@ -0,0 +1,58 @@
+import React from 'react'
+
+import styled from 'styled-components'
+
+import { bleu } from '../../styles/colors'
+
+const StyledSection = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    border-right: 2px dashed lightgrey;
+    :last-child{
+        border: none;
+    }
+    padding: 0rem 4rem;
+    margin: 3rem 0rem;
+
+    color: black;
+    line-height: 1.6;
+    p{
+        font-size: 1.6rem;
+        font-weight: 300;
+        margin: 0;
+    }
+    span{
+        color: ${bleu};
+        font-weight: 200;
+    }
+
+`;
+
+const ImgContainer = styled.div`
+    width: 18rem;
+    margin-top: 3rem;
+    img{
+        width: 100%;
+        object-fit: cover;
+    }
+`;
+
+
+const Testimony = ({content1, content2, strong, name, logo}) => {
+    return(
+        <StyledSection>
+            <p> {content1} 
+                <strong> {strong} </strong>
+                {content2}
+            </p>
+            <span> {name} </span>
+            <ImgContainer>
+                <img src={logo} />
+            </ImgContainer>
+        </StyledSection>
+    );
+}
+
+export default Testimony;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,14 +4,26 @@ import Layout from "../components/layout/layout"
 import SEO from "../components/seo"
 
 import styled from 'styled-components'
+import Slider from 'react-styled-carousel'
 
 import Head from "../components/home/head"
 import Section from '../components/home/section'
 import Team from '../components/home/team'
+import Testimony from '../components/home/testimony'
 
 const Sections = styled.div`
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
+`;
+
+const StyledSlider = styled(Slider)`
+    margin: 2rem 6rem;
+    padding: 0rem 8rem;
+`;
+
+const Testimonies = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
 `;
 
 const IndexPage = () => (
@@ -30,6 +42,40 @@ const IndexPage = () => (
             picture={require("../images/home/from-edge-workload.png")} />
     </Sections>
     <Team/>
+    <StyledSlider autoSlide={false} showDots={false} cardsToShow={1}>
+        <Testimonies>
+            <Testimony content1='" We at NAV are using Istio to migrate workloads to public clouds. It is critical that we do so without impacting reliability or security. We were facing challenges around networking APIs, HTTPS handling and modeling service interactions outside the Istio mesh.'
+                strong='With the help of  Tetrate, we’ve adopted Istio incrementally and are now successfully running Istio in production. "'
+                name="— Norwegian Labour and Welfare Administration (NAV)"
+                logo={require("../images/home/testimonies/nav.png")}/>
+            <Testimony content1='"Google Cloud’s leadership in service mesh technologies stems from our goal of making hybrid cloud seamless for customers.'
+                strong='The Tetrate team brings strong expertise in this space for cloud-native and brownfield deployments.'
+                content2='We look forward to collaborating with Tetrate to help our joint customers harness these technologies for digital transformation."'
+                name="— Prajakta Joshi, Senior Product Manager, Google Cloud"
+                logo={require("../images/home/testimonies/google-cloud.png")}/>
+        </Testimonies>
+        <Testimonies>
+            <Testimony content1='" As Envoy becomes more widely adopted, we expect to see the emergence of a consistent control plane that manages Envoy across different platforms and abstractions. Interoperability will be a key success factor. '
+                strong='Having deep roots within the open source community gives Tetrate instant credibility among cloud-first companies."'
+                name="— Matt Klein, Creator of Envoy; Engineer, Lyft"
+                logo={require("../images/home/testimonies/lyft.png")}/>
+            <Testimony content1='"Service meshes are integral to organizations as they begin cloud native evolution. I’m thrilled to see the launch of Tetrate to bring their broad service mesh expertise to the enterprise.'
+                strong='They have been pioneers in the enterprise service mesh space through their contributions to Envoy, Istio and the broader CNCF ecosystem."'
+                name="— Chris Aniszczyk, CTO/COO of Cloud Native Computing Foundation"
+                logo={require("../images/home/testimonies/GCloud.png")}/>
+        </Testimonies>
+        <Testimonies>
+            <Testimony 
+                strong='"Tetrate is at the forefront of the cloud-native movement – managing microservices at scale.'
+                content2='The company’s leadership team is responsible for some of the most important innovations within the cloud-native space and we’re fortunate to partner with them to address this significant market need."'
+                name="— Deepak Jeevankumar, Managing Director, Dell Technology Capital"
+                logo={require("../images/home/testimonies/dell.jpg")}/>
+            <Testimony content1='"Tetrate offers enterprises the tools to implement cloud-native architectures in an effective and efficient manner.  Intel Capital believes strongly in the power of open source software to deliver cloud-native solutions at scale, and the '
+                strong='Tetrate team’s ongoing contributions to the Istio and Envoy projects continue to solidify them as leading, core community members"'
+                name="— Nick Washburn & Mark Rostick, Managing Directors, Intel Capital"
+                logo={require("../images/home/testimonies/intel.jpg")}/>
+        </Testimonies>
+    </StyledSlider>
   </Layout>
 )
 
